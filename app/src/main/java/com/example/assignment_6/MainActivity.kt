139d952d52ce47d1f1d076f74d735a817fe9c8f0
jaiV191 <w0845065@myscc.ca>
2025-02-27 17:20:07 -0500

    Assignment-6 done

---
+++ app/src/main/java/com/example/assignment_6/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.assignment_6
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var edit: EditText
+    private lateinit var text1: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        edit = findViewById(R.id.editText)
+        text1 = findViewById(R.id.textView3)
+    }
+
+    fun showText(view: View){
+        val newText = edit.text.toString()
+        text1.text = "Hello $newText"
+    }
+}
