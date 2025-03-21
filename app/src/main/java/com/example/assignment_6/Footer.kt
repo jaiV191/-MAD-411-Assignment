8fa744055af429df6a9588dcad0306e7761ac6dc
jaiV191 <w0845065@myscc.ca>
2025-03-21 12:31:50 -0400

    Fragment for header and footer

---
+++ app/src/main/java/com/example/assignment_6/Footer.kt
@@ -0,0 +1,16 @@
+package com.example.assignment_6
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FooterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.footer, container, false)
+    }
+}
